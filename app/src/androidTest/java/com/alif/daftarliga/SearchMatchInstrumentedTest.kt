62d7056ab2738424d5f460948ff5ee1f5e54a725
alif-dev <alifm.dev@gmail.com>
2020-01-22 12:46:57 +0800

    fix testSearchMatch Navigate up view hierarchy error

--- app/src/androidTest/java/com/alif/daftarliga/SearchMatchInstrumentedTest.kt
+++ app/src/androidTest/java/com/alif/daftarliga/SearchMatchInstrumentedTest.kt
@@ -1,6 +1,8 @@
 package com.alif.daftarliga
 
 import android.view.KeyEvent
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
@@ -10,7 +12,10 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import com.alif.daftarliga.R.id.*
 import com.alif.daftarliga.view.MainActivity
+import org.hamcrest.Description
+import org.hamcrest.Matcher
 import org.hamcrest.Matchers
+import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -26,19 +31,67 @@ class SearchMatchInstrumentedTest {
         onView(withId(main_tabs))
         onView(withText("Search")).perform(click())
         onView(withId(search_view_match))
+
         val appCompatImageView = onView(
             Matchers.allOf(
                 withClassName(Matchers.`is`("androidx.appcompat.widget.AppCompatImageView")),
-                withContentDescription("Search")
+                withContentDescription(R.string.content_desc_search),
+                childAtPosition(
+                    Matchers.allOf(
+                        withClassName(Matchers.`is`("android.widget.LinearLayout")),
+                        childAtPosition(
+                            withId(search_view_match),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
             )
         )
         appCompatImageView.perform(click())
+
         onView(withId(search_view_match))
             .perform(typeText("Liverpool"))
             .perform(pressKey(KeyEvent.KEYCODE_ENTER))
         onView(withId(rv_searched_matches))
             .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(8))
             .perform(click())
-        onView(withContentDescription("Navigate up")).perform(click())
+
+        val appCompatImageButton = onView(
+            Matchers.allOf(
+                withContentDescription(R.string.content_desc_navigate_up),
+                childAtPosition(
+                    Matchers.allOf(
+                        withId(action_bar),
+                        childAtPosition(
+                            withId(action_bar_container),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatImageButton.perform(click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
     }
 }
